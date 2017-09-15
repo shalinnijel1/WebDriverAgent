64a250b0d6dd15cd0e87f0626fb3a56222ce509f
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-09-15 17:47:36 +0200

    Update Travis scripts

---
+++ build-ipa.sh
@@ -0,0 +1,7 @@
+mkdir ./out
+xcodebuild -project WebDriverAgent.xcodeproj -scheme WebDriverAgentRunner -sdk iphoneos -configuration Release -derivedDataPath ./out
+mkdir -p ./out/ipa/Payload
+cp -r ./out/Build/Products/Release-iphoneos/WebDriverAgentRunner-Runner.app ./out/ipa/Payload/
+cd ./out/ipa
+zip -r ../WebDriverAgent-$TRAVIS_BUILD_NUMBER.zip .
+cd ../../
